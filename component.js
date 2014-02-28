55fa027739ee3ad50bf3e49cf0fa65b43797ccb1
Andrew Reedy <andrew.reedy@thecontrolgroup.com>
2014-02-28 03:03:51 -0800

    Create component.js

---
+++ component.js
@@ -0,0 +1,11 @@
+{
+  "name": "log",
+  "repo": "adamschwartz/log",
+  "version": "0.1.0",
+  "description": "Console.log with style",
+  "keywords": [],
+  "main": "log.js",
+  "scripts": [
+    "log.js"
+  ]
+}
